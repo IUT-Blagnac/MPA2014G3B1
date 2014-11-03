8bf53423d9781893b787f6ba36a35ad1ac1d5023
timotheeficat <timothee.ficat@laposte.net>
2014-11-03 16:46:38 +0100

    [skip ci]

--- sprint_5/src/MakeOPTIweb.java
+++ sprint_5/src/MakeOPTIweb.java
@@ -21,7 +21,7 @@ public class MakeOPTIweb {
 				"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />" ,
 				"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" ,
 				"<meta name=\"generator\" content=\"OPTIweb VOPTIweb\" />" ,
-				"<title>0.1 - V0.1</title>" ,
+				"<title>OPTIweb - V0.1</title>" ,
 				"<link rel=\"stylesheet\" href=\"http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css\" />" ,
 				"<link rel=\"stylesheet\" href=\"http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css\" />" ,
 				"<script src=\"http://code.jquery.com/jquery-1.9.1.min.js\"></script>" ,
